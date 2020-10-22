8642e84d955eeb1bb9d717f56366dab4590a9d1e
Aamir Mahmood <mohdaamir8182@gmail.com>
2020-10-22 16:31:49 -0700

    remove duplicate elements from an array

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -1,11 +1,17 @@
+//********************DELETE DUPLICATES FROM AN ARRAY*************************
+
 const arr = [1,2,3,3,3];
 const newArr = [];
 
 const deleteDup = () => {
+
+    //METHOD 1
   
-//    return arr.filter((data,index)=>{
-//         return arr.indexOf(data) === index;
-//       })
+   return arr.filter((data,index)=>{
+        return arr.indexOf(data) === index;
+      })
+
+    //METHOD 2
 
     arr.forEach((item , index) => {
         if(!newArr.includes(item)){
